9bd7596f8718264f494d7bc48a3cd38bce2dec59
Adam Beynon <adam.beynon@gmail.com>
2012-09-14 10:10:45 +0100

    Document todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,10 +4,14 @@ class Todo < Model
   attribute :title
   attribute :completed
 
+  # All active (not completed) todos
+  # @return [Array<Todo>]
   def self.active
     select { |todo| !todo.completed }
   end
 
+  # All completed todos
+  # @return [Array<Todo>]
   def self.completed
     select { |todo| todo.completed }
   end

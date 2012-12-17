534d3f950ef124d96175735b1685209abc5f57cd
Adam Beynon <adam.beynon@gmail.com>
2012-12-17 22:10:23 +0000

    Remove duplicate code

--- app/lib/model.rb
+++ app/lib/model.rb
@@ -5,8 +5,7 @@ module Vienna
     extend Enumerable
 
     def self.attribute(name)
-      define_method(name) { @attributes[name] }
-      define_method("#{name}=") { |val| @attributes[name] = val }
+      string name
     end
 
     def self.inherited(base)
@@ -47,22 +46,6 @@ module Vienna
 
     def self.each(&block)
       @_models.each { |m| block.call m }
-    end
-    
-    def initialize(attributes={})
-      @attributes = {}
-
-      attributes.each do |name, val|
-        __send__ "#{name}=", val if respond_to? "#{name}="
-      end
-    end
-
-    def [](name)
-      @attributes[name]
-    end
-
-    def []=(name, value)
-      @attributes[name] = value
     end
 
     def save

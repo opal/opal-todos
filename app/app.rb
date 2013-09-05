ef047465efa4a7460f62ef9b8d43cd4ff6e521da
Adam Beynon <adam.beynon@gmail.com>
2013-09-05 18:12:30 +0100

    Introduce Vienna::Application for controlling initializer flow

--- app/app.rb
+++ app/app.rb
@@ -12,7 +12,35 @@ require 'templates/todo'
 
 require 'models/todo'
 
+module Vienna
+  class << self
+    attr_accessor :application
+  end
+
+  class Application
+    class << self
+      def inherited(base)
+        Vienna.application ||= base.instance
+      end
+
+      def method_missing(sym, *args)
+        instance.__send__(sym, *args)
+      end
+
+      def instance
+        @instance ||= self.new
+      end
+    end
+  end
+end
+
+class Application < Vienna::Application
+  def run
+    @app_view = AppView.new
+  end
+end
+
 # when document is ready, lets go!
 Document.ready? do
-  App = AppView.new
+  Application.run
 end

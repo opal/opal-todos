dcbcd85576f52486a40c2eed54b75641bf2cbe7f
Lucas Caton <lucascaton@gmail.com>
2015-03-13 23:17:24 +1000

    Renaming todo_spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,12 +2,10 @@ require 'opal'
 require 'vienna'
 require 'models/todo'
 
-RSpec.configure do |config|
-  config.before { Todo.reset! }
-end
-
 describe Todo do
   before do
+    Todo.reset!
+
     @task_a = Todo.create(title: 'Foo', completed: true)
     @task_b = Todo.create(title: 'Bar', completed: false)
     @task_c = Todo.create(title: 'Baz', completed: true)
@@ -15,13 +13,13 @@ describe Todo do
   end
 
   describe '.active' do
-    it "returns all non-completed todos" do
+    it 'returns all non-completed todos' do
       expect(Todo.active).to eq([@task_b, @task_d])
     end
   end
 
   describe '.completed' do
-    it "returns all completed todos" do
+    it 'returns all completed todos' do
       expect(Todo.completed).to eq([@task_a, @task_c])
     end
   end
